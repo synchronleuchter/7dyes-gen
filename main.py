4424427477c2dba169e39bd2ae9f3bd23b7aeb4c
Synchronleuchter <x-heiko@skullbyte.de>
2022-11-25 18:54:55 +0100

    Add output image with 2 dimensions

--- main.py
+++ main.py
@@ -2,16 +2,21 @@ from PIL import Image
 import numpy as np
 
 
-def sample_hsv_cone(hue_steps=26, saturation_step_depth=2, value_step_depth=2):
+def identity(x):
+    return x
+
+
+def sample_hsv_cone(hue_steps=26, saturation_step_depth=2, value_step_depth=2, s_sampler=identity, v_sampler=identity):
     result_list = []
     delta_h = 360.0 / hue_steps
 
     for h_step in range(0, hue_steps):
-        result_list.extend(sample_hue_triangle(h_step * delta_h, saturation_step_depth, value_step_depth))
+        result_list.extend(
+            sample_hue_triangle(h_step * delta_h, saturation_step_depth, value_step_depth, s_sampler, v_sampler))
     return result_list
 
 
-def sample_hue_triangle(h, saturation_step_depth=2, value_step_depth=2):
+def sample_hue_triangle(h, saturation_step_depth=2, value_step_depth=2, s_sampler=identity, v_sampler=identity):
     result_list = []
     saturation_steps = depth_to_steps(saturation_step_depth)
     value_steps = depth_to_steps(value_step_depth)
@@ -20,19 +25,19 @@ def sample_hue_triangle(h, saturation_step_depth=2, value_step_depth=2):
 
     # Start at 1 because greyscale repeats itself otherwise
     for s_step in range(1, saturation_steps):
-        s = delta_s * s_step
+        s = s_sampler(delta_s * s_step)
         for v_step in range(1, value_steps):
-            v = delta_v * v_step
+            v = v_sampler(delta_v * v_step)
             result_list.append([h, s, v])
     return result_list
 
 
-def sample_grayscale_line(value_step_depth=2):
+def sample_grayscale_line(value_step_depth=2, v_sampler=identity):
     result_list = []
     value_steps = depth_to_steps(value_step_depth)
     delta_v = delta_with_borders(value_steps)
     for v_step in range(0, value_steps):
-        result_list.append([0, 0, delta_v * v_step])
+        result_list.append([0, 0, v_sampler(delta_v * v_step)])
     return result_list
 
 
@@ -78,8 +83,16 @@ def rounded_colors(lst):
 
 
 if __name__ == '__main__':
-    cone = sample_hsv_cone(36, 2, 3)
-    line = sample_grayscale_line(3)
+    s_step_depth = 2
+    v_step_depth = 2
+    h_steps = 72
+
+    v_samp = identity
+    s_samp = identity
+
+    cone = sample_hsv_cone(h_steps, s_step_depth, v_step_depth, s_samp, v_samp)
+    line = sample_grayscale_line(v_step_depth, v_samp)
+
     print("HSV color cone:")
     print(cone)
     print("HSV grayscale line:")
@@ -91,13 +104,18 @@ if __name__ == '__main__':
     rgb_line = list(map(hsv_to_rgb, line))
     print(rounded_colors(rgb_line))
 
-    img_arr = np.zeros([100, len(cone) + len(line), 3], dtype=np.uint8)
-    for i in range(0, len(rgb_cone)):
-        img_arr[:, i:i+1] = rgb_cone[i]
+    s_steps = 2 ** v_step_depth
+
+    img_arr = np.zeros([s_steps, int(len(cone) / s_steps) + len(line), 3],
+                       dtype=np.uint8)
+
+    for i in range(0, int(len(rgb_cone) / s_steps)):
+        for j in range(0, s_steps):
+            img_arr[j, i] = rgb_cone[i * s_steps + j]
 
     for i in range(0, len(rgb_line)):
-        j = len(rgb_cone) + i
-        img_arr[:, j:j+1] = rgb_line[i]
+        j = int(len(rgb_cone) / s_steps) + i
+        img_arr[:, j:j + 1] = rgb_line[i]
 
     img = Image.fromarray(img_arr)
     img.save('out.png')
