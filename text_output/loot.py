b776e04ddfeed0aa96d43edc19dcb4dec48497dc
Synchronleuchter <x-heiko@skullbyte.de>
2024-08-31 21:44:27 +0200

    Remove dyes from armor

--- text_output/loot.py
+++ text_output/loot.py
@@ -4,7 +4,6 @@ from text_output import common
 prefix = '''<configs>'''
 
 suffix = '''<append xpath="/lootcontainers/lootgroup[starts-with(@name, 'groupLaundry0')]"> <item name="dyes" count="1" loot_prob_template="low" /></append>
-<append xpath="/lootcontainers/lootgroup[@name='groupArmorParts']"> <item name="dyes" count="1" loot_prob_template="low" /></append>
 </configs>'''
 
 
